06dd791970d3f37c77b5824bb567da48ed7f9fe3
GurpreetJhamat <gjhammt@gmail.com>
2020-04-17 23:55:25 -0400

    Next commit

--- web-servers/src/app.js
+++ web-servers/src/app.js
@@ -6,8 +6,27 @@ const port = 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Handelbars',
+        name: 'Gurpreet',
+    });
+});
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Gurpreet',
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'This is an exapmle message!',
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
