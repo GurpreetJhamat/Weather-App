c4e1aa9cc47ed8882ba5f9ddda1c085f0ffc1857
GurpreetJhamat <gjhammt@gmail.com>
2020-05-11 18:01:38 -0400

    Almost Done

--- web-servers/public/js/app.js
+++ web-servers/public/js/app.js
@@ -20,7 +20,9 @@ const feel = document.querySelector('#feel');
 const docBody = document.body;
 const header = document.getElementById('header')
 
-
+docBody.style.backgroundSize = "cover";
+docBody.style.backgroundRepeat = "no-repeat"
+docBody.style.backgroundPosition = "center center"
 const getData = (e) => {
     e.preventDefault()
     const location = search.value 
@@ -55,96 +57,155 @@ const getData = (e) => {
             time.textContent = data.forecast.timeDate;
             cloud.textContent = `${data.forecast.cloudy}% Cloudy`;
             feel.textContent = `${data.forecast.feels}°C Feels Like`
-            // console.log(data);
-            // console.log(data.forecast.description)
-            // console.log(img.src);
-            // console.log(weatherDescription)
-            // console.log(data.forecast);
-            // current.src = e.target.src;
         }
 
+        let weatherDescription = data.forecast.description;
+        // let weatherDescription = "Blizzard";
+        let isDay = data.forecast.dayNight;
+        // let isDay = "yes";
+        // console.log(isDay)
+console.log(weatherDescription)
+        let description = weatherDescription.split(' ')
 
 
-            let weatherDescription = data.forecast.description;
-            // let weatherDescription = "Light Rain";
-            // let weatherDescription = "Light Rain Shower";
-            // let weatherDescription = "Rain, Mist";
-            // let weatherDescription = "Patchy Rain Possible";
-            console.log(weatherDescription)
-            let isDay = data.forecast.dayNight;
-            // let isDay = "yes"
-            console.log(isDay)
-
+//FOR OVERCAST
         if(weatherDescription === "Overcast" && isDay === "yes"){
             docBody.style.backgroundImage = "url('../images_weather/overcast-day.jpg')";
         } 
-        else if(weatherDescription === "Overcast" && isDay === "no"){
+        if(weatherDescription === "Overcast" && isDay === "no"){
             docBody.style.backgroundImage = "url('../images_weather/overcast-night.jpg')";
         } 
-        else if(weatherDescription === "Sunny" && isDay === "yes"){
+      
+//FOR SUNNY        
+        if(weatherDescription === "Sunny" && isDay === "yes"){
             docBody.style.backgroundImage = "url('../images_weather/sunny.jpg')";
         } 
-        else if(weatherDescription === "Snow" && isDay === "yes"){
+
+//FOR SNOW        
+        if(weatherDescription === "Snow" && isDay === "yes"){
             docBody.style.backgroundImage = "url('../images_weather/snow-day.jpg')";
-        } 
-        else if(weatherDescription === "Snow" && isDay === "no"){
+        }      
+        if(weatherDescription === "Snow" && isDay === "no"){
             docBody.style.backgroundImage = "url('../images_weather/snow-night.jpg')";
-        } 
-        else if(weatherDescription === "Clouds" && isDay === "yes"){
-            docBody.style.backgroundImage = "url('../images_weather/cloudy-day.jpg')";
         }
-        else if(weatherDescription === "Clouds" && isDay === "no"){
-            docBody.style.backgroundImage = "url('../images_weather/cloudy-night.jpg')";
+        if(weatherDescription === "Blizzard" && isDay === "yes"){
+            docBody.style.backgroundImage = "url('../images_weather/snow-day.jpg')";
+        }      
+        if(weatherDescription === "Blizzard" && isDay === "no"){
+            docBody.style.backgroundImage = "url('../images_weather/snow-night.jpg')";
         }
-        else if(weatherDescription === "Partly Cloudy" && isDay === "yes"){
+
+//FOR PARTLY CLOUDY
+        if(weatherDescription === "Partly cloudy" && isDay === "yes"){
             docBody.style.backgroundImage = "url('../images_weather/partly-cloudy-day.jpg')";
         }
-        else if(weatherDescription === "Partly Cloudy" && isDay === "no"){
+        if(weatherDescription === "Partly cloudy" && isDay === "no"){
             docBody.style.backgroundImage = "url('../images_weather/partly-cloudy-night.jpg')";
-        }   
-        else if(weatherDescription === "Light Rain" && isDay === "yes"){
-            docBody.style.backgroundImage = "url('../images_weather/rain-day.jpg')";
-        }
-        else if(weatherDescription === "Light Rain" && isDay === "no"){
-            docBody.style.backgroundImage = "url('../images_weather/rain-night.jpg')";
         } 
-        else if(weatherDescription === "Clear" && isDay === "yes"){
+
+//FOR CLEAR WEATHER
+        if(weatherDescription === "Clear" && isDay === "yes"){
             docBody.style.backgroundImage = "url('../images_weather/clear-day.jpg')";
         } 
-        else if(weatherDescription === "Clear" && isDay === "no"){
+        if(weatherDescription === "Clear" && isDay === "no"){
             docBody.style.backgroundImage = "url('../images_weather/clear-night.jpg')";
         }
-        else if(weatherDescription === "Thunderstorm" && isDay === "yes" || isDay == "no" ){
-            docBody.style.backgroundImage = "url('../images_weather/clear-night.jpg')";
-        }
-        else if(weatherDescription === "Fog" && isDay === "yes"){
+
+//FOR HAZZY AND FOGGY WEATHER
+
+
+let fogKeyWords = ["Fog", "Mist", "Fog,", "Mist," ]
+
+// if(weatherDescription){
+//     for(let i = 0; i < description.length; i++) {
+//         if(fogKeyWords.includes(description[i])) {
+//             console.log(description[i], ': we have a match')
+//             docBody.style.backgroundImage = "url('../images_weather/fog-day.jpg')";
+//             docBody.style.backgroundSize = "cover";
+//         }
+//     }
+//     // keyWords.push(weatherDescription);
+//     console.log(description);
+//     console.log(fogKeyWords);
+// }
+        if(weatherDescription === "Fog" && isDay === "yes"){
             docBody.style.backgroundImage = "url('../images_weather/fog-day.jpg')";
         } 
-        else if(weatherDescription === "Fog" && isDay === "no"){
+        if(weatherDescription === "Fog" && isDay === "no"){
             docBody.style.backgroundImage = "url('../images_weather/fog-night.jpg')";
         }
-        else if(weatherDescription === "Haze" && isDay === "yes"){
+        if(weatherDescription === "Haze" && isDay === "yes"){
             docBody.style.backgroundImage = "url('../images_weather/fog-day.jpg')";
         } 
-        else if(weatherDescription === "Haze" && isDay === "no"){
+        if(weatherDescription === "Haze" && isDay === "no"){
             docBody.style.backgroundImage = "url('../images_weather/fog-night.jpg')";
         }
-        
-        
-        else{console.log('nothing matching found')}
+        if(weatherDescription === "Fog, Mist" && isDay === "yes"){
+            docBody.style.backgroundImage = "url('../images_weather/fog-day.jpg')";
+        }
+        if(weatherDescription === "Fog, Mist" && isDay === "no"){
+            docBody.style.backgroundImage = "url('../images_weather/fog-night.jpg')";
+        }
+
+//FOR RAIN & THUNDERSTORM
+            let keyWords = ['Thunderstorm','Rain', 'Mist', 'with', 'Rain,', 'Mist,'];
+            if(weatherDescription){
+                for(let i = 0; i < description.length; i++) {
+                    if(keyWords.includes(description[i]) && isDay === "yes") {
+                        console.log(description[i], ': we have a match')
+                        docBody.style.backgroundImage = "url('../images_weather/rain-lightning.jpg')";
+                        docBody.style.backgroundSize = "cover";
+                    } 
+                }
+                
+            }
+
+
+// FOR RAIN
+            let rainKeyWords = ['Rain','Light', 'Mist', 'Shower', 'Drizzzle', 'Rain,', 'Mist,', 'Moderate Rain at times',]
+            if(weatherDescription){
+                for(let i = 0; i < description.length; i++) {
+                    if(rainKeyWords.includes(description[i]) && isDay === "yes") {
+                        console.log(description[i], ': we have a match')
+                        docBody.style.background = "url('../images_weather/rain-day.jpg')"
+                        docBody.style.backgroundSize = "cover";
+                    } else if(rainKeyWords.includes(description[i]) && isDay === "no"){
+                        docBody.style.background = "url('../images_weather/rain-night.jpg')"
+                        docBody.style.backgroundSize = "cover";
+                    }
+                }
+            }
+
+
+
 
 
          
-
-
+        if(weatherDescription === "Clouds" && isDay === "yes"){
+            docBody.style.backgroundImage = "url('../images_weather/cloudy-day.jpg')";
+        }
+        if(weatherDescription === "Clouds" && isDay === "no"){
+            docBody.style.backgroundImage = "url('../images_weather/cloudy-night.jpg')";
+        }
+          
+        if(weatherDescription === "Smoke" && isDay === "yes"){
+            docBody.style.backgroundImage = "url('../images_weather/smoke-day.jpg')";
+        }
+        if(weatherDescription === "Smoke" && isDay === "no"){
+            docBody.style.backgroundImage = "url('../images_weather/smoke-night.jpg')";
+        } 
         
+        // else if(weatherDescription === "Thunderstorm" && isDay === "yes" || isDay == "no" ){
+        //     docBody.style.backgroundImage = "url('../images_weather/thunderstorm.jpg')";
+        // }
+        
+        
+        
+        // else{console.log('nothing matching found')}
+
+
+                 
     })
 })
 }
-
-// const hello = () => {
-//     console.log(weatherDescription);
-// }
-
 weatherForm.addEventListener('submit', getData);
-// weatherForm2.addEventListener('submit', getData);
